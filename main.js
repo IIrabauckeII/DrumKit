050cbb0a9462b52ff399a1855b5b5515ca091674
IIrabauckeII <janis@daniel.in-berlin.de>
2020-04-10 20:33:47 +0200

    added visual feedback for keydown

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ buttons.forEach((button) => {
 document.addEventListener('keydown', (e) => {
   if (['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'].includes(e.key)
 ) {
-    let target = document.getElementById(e.key);
-    target.play();
-
+    let targetSound = document.getElementById(e.key + 'Sound');
+    targetSound.play();
+/*    let targetKey = document.getElementById(e.key + 'Key');
+    targetKey.style.background = "rgba(252,166,34,0.4)";
+    targetKey.style.transform = "scale(1.15)";
+    targetKey.style['border-color'] = "black";*/
+    document.getElementById(e.key + 'Key').className = "keyDown";
+    setTimeout(function(){
+      document.getElementById(e.key + 'Key').className = "key"}, 400);
   }
 });
